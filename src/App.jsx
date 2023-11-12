de3d2fb81988c875ac15d87f8790424278d3f38f
emmavie <75275724+emmavie@users.noreply.github.com>
2023-11-12 15:18:12 -0500

    Adding basic info screen
    - slight css changes
    - creating the carousel for basic info screen

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import LandingPage from "./pages/LandingPage";
 import SignUp from "./pages/SignUp";
 import EmailVerify from "./pages/EmailVerify";
 import LogIn from "./pages/LogIn";
+import BasicInfo from "./pages/BasicInfo";
+import Changes from "./pages/Changes";
 import { Route, Routes } from "react-router-dom";
 
 function App() {
@@ -14,6 +16,9 @@ function App() {
         <Route path="/SignUp" element={<SignUp />}></Route>
         <Route path="/EmailVerify" element={<EmailVerify />}></Route>
         <Route path="/LogIn" element={<LogIn />}></Route>
+        <Route path="/BasicInfo" element={<BasicInfo />}></Route>
+        <Route path="/Changes" element={<Changes />}></Route>
+
       </Routes>
     </>
   );
